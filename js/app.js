220a289f0fbb0b03824bf19b3ad73bc2ea393528
Richard Ludwig <rmludwig@rmlsn.net>
2015-02-17 08:09:38 -0600

    Updated code to more closely follow the google javascript style guide. Updated index in an effort to resolve issues with IE11.

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ var Enemy = function(name, initialSpeed, stack, sprite) {
     this.laneSelection();
 
     // Log instantiation
-    console.log("Welcoming the enemy "+this.name+" to the game.");
-}
+    console.log('Welcoming the enemy '+this.name+' to the game.');
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -45,8 +45,8 @@ Enemy.prototype.update = function(dt) {
     }
 
     // DEBUG logging level 2 (verbose)
-    if(debug > 1){console.log("calling enemy update x="+this.x+" y="+this.y)}; 
-}
+    if(debug > 1){console.log('calling enemy update x='+this.x+' y='+this.y)}; 
+};
 
 // When instantiated, and when at end of row a new random lane
 // should be set for any Enemy
@@ -58,8 +58,8 @@ Enemy.prototype.laneSelection = function(lane) {
     this.y = (this.lane * rowHeight) - 35 + (this.stack * stackHeight);
     
     // DEBUG level 1
-    if(debug > 0){console.log("Enemy "+this.name+" is now in lane "+this.lane)};
-}
+    if(debug > 0){console.log('Enemy '+this.name+' is now in lane '+this.lane)};
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -71,8 +71,8 @@ Enemy.prototype.reset = function() {
     this.speed = this.initialSpeed;
 
     // DEBUG level 1
-    if(debug > 0){console.log("Enemy "+this.name+" was reset to start values")};
-}
+    if(debug > 0){console.log('Enemy '+this.name+' was reset to start values')};
+};
 
 
 
@@ -90,7 +90,7 @@ var Player = function(loc){
     this.reset();
 
     // Log instatiation
-    console.log("Welcoming a new player to the game.");
+    console.log('Welcoming a new player to the game.');
 };
 
 // Update player and handle mechanics of collisions, lives
@@ -102,7 +102,7 @@ Player.prototype.update = function(){
     allEnemies.forEach(function(enemy) {
         if (enemy.lane === player.row && (enemy.x + 78) > player.x && enemy.x < (player.x + 80)) {
             // Log the collision
-            console.log("THE BUG "+enemy.name+" GOT THE PLAYER at loc="+player.loc+"!!!");
+            console.log('An Enemy named '+enemy.name+' GOT THE PLAYER at loc='+player.loc+'!!!');
             
             // Put player out of bounds, then next move will bring them to start position
             player.loc = 32;
@@ -112,16 +112,16 @@ Player.prototype.update = function(){
 
             // Is the game over? (out of lives)
             if (player.lives < 0) {
-                player.message = "GAME OVER";
+                player.message = 'GAME OVER';
 
                 // Temporarily set lives back to 0 so when rendered -1 is not seen
                 player.lives = 0;
 
                 // Log GAME OVER
-                console.log(player.message)
+                console.log(player.message);
             }
             else {
-                player.message = "Try again";
+                player.message = 'Try again';
             }
             
             // Pause game
@@ -143,8 +143,8 @@ Player.prototype.update = function(){
         this.moveTo(this.loc);
         this.score = this.score + perLevelScore;
         currentGameLevel++;
-        this.message = "Level "+currentGameLevel;
-        console.log("Player GOAL!!!");
+        this.message = 'Level '+currentGameLevel;
+        console.log('Player GOAL!!!');
 
         // Replace powerUps after score
         powerUp.moveToRandomLoc();
@@ -155,27 +155,27 @@ Player.prototype.update = function(){
             perLevelScore = 2;
             tricky.reset();
             allEnemies.push(micky,nicky);
-            this.message = "Pink Levels";
+            this.message = 'Pink Levels';
             this.sprite = 'images/char-pink-girl.png';
-            console.log("Difficulty Pink");
+            console.log('Difficulty Pink');
         }
         else if (currentGameLevel === 10) {
             perLevelScore = 3;
             tricky.reset();
             sticky.speed = sticky.speed + 5;
             allEnemies.push(ricky);
-            this.message = "Kitty Levels";
+            this.message = 'Kitty Levels';
             this.sprite = 'images/char-cat-girl.png';
-            console.log("Difficulty Kitty");
+            console.log('Difficulty Kitty');
         }
         else if (currentGameLevel === 15) {
             perLevelScore = 5;
             tricky.reset();
             nicky.speed = nicky.speed + 5;
             allEnemies.push(picky);
-            this.message = "Mage Levels";
+            this.message = 'Mage Levels';
             this.sprite = 'images/char-horn-girl.png';
-            console.log("Difficulty Mage");
+            console.log('Difficulty Mage');
         }
         // Boss levels
         else if (currentGameLevel === 20) {
@@ -183,9 +183,9 @@ Player.prototype.update = function(){
             tricky.reset();
             picky.speed = picky.speed + 5;
             allEnemies.push(steve);
-            this.message = "Boss Levels";
+            this.message = 'Boss Levels';
             this.sprite = 'images/char-princess-girl.png';
-            console.log("Difficulty BOSS LEVELS");
+            console.log('Difficulty BOSS LEVELS');
         }
         else {
             // Minor difficulty increase for each level.
@@ -217,7 +217,7 @@ Player.prototype.reset = function(){
     this.sprite = 'images/char-boy.png';
 
     // Start of game attributes
-    this.message = "Jump to WATER";
+    this.message = 'Jump to WATER';
     this.score = 0;
     this.lives = 3;
 
@@ -232,8 +232,8 @@ Player.prototype.reset = function(){
     maxLanes = 3;
 
     // Log new game
-    console.log("Player was reset.")
-}
+    console.log('Player was reset.');
+};
 
 // Move a player to a new position on game board
 Player.prototype.moveTo = function(loc){
@@ -246,7 +246,7 @@ Player.prototype.moveTo = function(loc){
     this.x = 101 * this.column;
 
     // DEBUG level 1
-    if(debug > 0){console.log("For player moveTo changed values to row="+this.row+" loc="+this.loc+" x="+this.x+" y="+this.y)};
+    if(debug > 0){console.log('For player moveTo changed values to row='+this.row+' loc='+this.loc+' x='+this.x+' y='+this.y)};
 };
 
 // Along with player render also render player messages.
@@ -255,25 +255,25 @@ Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     // Render the player score and lives always
-    ctx.strokeStyle = "#000";
-    ctx.fillStyle = "#FFF";
-    ctx.font = "bold 10pt Courier New";
-    ctx.textAlign = "left";
-    ctx.fillText("Score: "+this.score, 10, 70);
-    ctx.fillText("Lives: "+this.lives, 111, 70);
-    ctx.fillText("Level: "+currentGameLevel, 414, 70);
+    ctx.strokeStyle = '#000';
+    ctx.fillStyle = '#FFF';
+    ctx.font = 'bold 10pt Courier New';
+    ctx.textAlign = 'left';
+    ctx.fillText('Score: '+this.score, 10, 70);
+    ctx.fillText('Lives: '+this.lives, 111, 70);
+    ctx.fillText('Level: '+currentGameLevel, 414, 70);
 
     // Render the player message on the canvas as needed
     if (this.message.match(/\w/)) {
-        ctx.font = "bold 36pt Comic Sans MS";
-        ctx.lineWidth = "2";
-        ctx.textAlign = "center";
+        ctx.font = 'bold 36pt Comic Sans MS';
+        ctx.lineWidth = '2';
+        ctx.textAlign = 'center';
         ctx.fillText(this.message, 250, 445);
         ctx.strokeText(this.message, 250, 445);
     }
 
     // DEBUG logging level 2 or higher
-    if(debug > 1){console.log("Calling player render")};
+    if(debug > 1){console.log('Calling player render')};
 };
 
 // As key strokes occure they will be processed against the 
@@ -288,8 +288,8 @@ Player.prototype.handleInput = function(move){
     // paused. This would be better if not using JS confirm
     // dialogue. But for now it functions as needed. Maybe
     // for fun later I will create a custom in game dialogue.
-    if (player.message === "GAME OVER") {
-        var newGame = confirm("Do you want to start a new game?");
+    if (player.message === 'GAME OVER') {
+        var newGame = confirm('Do you want to start a new game?');
         if (newGame !== true) {
             freeze = 1;
             return;
@@ -304,19 +304,21 @@ Player.prototype.handleInput = function(move){
         }
     }
 
-    // Move in requested direction if not moving out of bounds
-    if (move === "up" && this.loc > 4) {
+    // Move in requested direction if not moving out of bounds.
+    // Move is accomplished by modifying the loc parameter as needed.
+    if (move === 'up' && this.loc > 4) {
         this.loc = this.loc - 5;
     }
-    else if (move === "down" && this.loc < 25) {
+    else if (move === 'down' && this.loc < 25) {
         this.loc = this.loc + 5;
     }
-    else if (move === "left" && (this.loc % 5 !== 0)) {
+    else if (move === 'left' && (this.loc % 5 !== 0)) {
         this.loc = this.loc - 1;
     }
-    else if (move === "right" && (this.loc % 5 !== 4)) {
+    else if (move === 'right' && (this.loc % 5 !== 4)) {
         this.loc = this.loc + 1;
     }
+    
     // out of bounds recovery
     if (this.loc > 29 || this.loc < 0) {
         this.loc = 27;
@@ -324,10 +326,10 @@ Player.prototype.handleInput = function(move){
     this.moveTo(this.loc);
 
     // Reset message to blank
-    this.message = " ";
+    this.message = ' ';
 
     // DEBUG logging
-    if(debug > 0){console.log("Moving player via handleInput argument "+move+" to new location of "+this.loc)};
+    if(debug > 0){console.log('Moving player via handleInput argument '+move+' to new location of '+this.loc)};
 };
 
 
@@ -339,7 +341,7 @@ var PowerUp = function(type){
     this.reset();
 
     // DEBUG level 1 log instatiation
-    if(debug > 0){console.log("Added the PowerUp")};
+    if(debug > 0){console.log('Added the PowerUp')};
 };
 
 // Reset the player after game restart
@@ -356,8 +358,8 @@ PowerUp.prototype.reset = function(){
     this.resetStar();
 
     // DEBUG level 1 when reset
-    if(debug > 0){console.log("PowerUp was reset.")};
-}
+    if(debug > 0){console.log('PowerUp was reset.')};
+};
 
 // Because star bonus's have special characteristics this 
 // is needed to reset the star bonus after timeout or
@@ -374,8 +376,8 @@ PowerUp.prototype.resetStar = function(){
     }
 
     // DEBUG level 1 when star was reset
-    if(debug > 0){console.log("PowerUp STAR was reset.")};
-}
+    if(debug > 0){console.log('PowerUp STAR was reset.')};
+};
 
 // Set a PowerUps random location outside of water and player start
 PowerUp.prototype.moveToRandomLoc = function(){
@@ -411,7 +413,7 @@ PowerUp.prototype.moveToRandomLoc = function(){
     this.timeout = now + (this.seconds * 1000);
 
     // DEBUG level 1
-    if(debug > 0){console.log("For PowerUp the moveTo function changed values to seconds="+this.seconds+" row="+this.row+" loc="+this.loc+" x="+this.x+" y="+this.y)};
+    if(debug > 0){console.log('For PowerUp the moveTo function changed values to seconds='+this.seconds+' row='+this.row+' loc='+this.loc+' x='+this.x+' y='+this.y)};
 };
 
 // Set a PowerUp type randomly
@@ -440,7 +442,7 @@ PowerUp.prototype.setRandomPowerUpType = function(){
     }
 
     // DEBUG level 1
-    if(debug > 0){console.log("New powerup type set to "+this.type)};
+    if(debug > 0){console.log('New powerup type set to '+this.type)};
 };
 
 // Along with player render also render player messages.
@@ -456,7 +458,7 @@ PowerUp.prototype.render = function(){
             ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
             // DEBUG logging level 2 or higher
-            if(debug > 1){console.log("Calling powerUp render")};
+            if(debug > 1){console.log('Calling powerUp render')};
         }
         else {
             this.visible = 0;
@@ -473,27 +475,27 @@ PowerUp.prototype.collectReward = function(){
     // Provide different behavior for different types
     // Blue (type 1 - 4) 1 point.
     if (this.type > 0 && this.type < 5) {
-        player.message = "BONUS";
+        player.message = 'BONUS';
         player.score++;
-        if(debug > 0){console.log("One point bonus")};
+        if(debug > 0){console.log('One point bonus')};
     }
     // Green (type 5 and 6) 2 points.
     else if (this.type > 4 && this.type < 7) {
-        player.message = "2 x BONUS";
+        player.message = '2 x BONUS';
         player.score = player.score + 2;
-        if(debug > 0){console.log("Two point bonus")};
+        if(debug > 0){console.log('Two point bonus')};
     }
     // Orange (type 7) 5 points.
     else if (this.type === 7) {
-        player.message = "5 x BONUS";
+        player.message = '5 x BONUS';
         player.score = player.score + 5;
-        if(debug > 0){console.log("Five point bonus")};
+        if(debug > 0){console.log('Five point bonus')};
     }
     // Heart is 1 more life
     else if (this.type === 8) {
-        player.message = "1 UP";
+        player.message = '1 UP';
         player.lives++;
-        if(debug > 0){console.log("Extra life")};
+        if(debug > 0){console.log('Extra life')};
     }
     // Star gives you five bonus's. Each one has higher
     // points and a quicker timeout.
@@ -501,12 +503,12 @@ PowerUp.prototype.collectReward = function(){
         player.score = player.score + this.starScore;
 
         // DEBUG level 1 for monitoring star bonus mechanics
-        if(debug > 0){console.log("Star bonus points "+this.starScore+" timeout "+this.seconds+" and count "+this.starCount)};
+        if(debug > 0){console.log('Star bonus points '+this.starScore+' timeout '+this.seconds+' and count '+this.starCount)};
         
         // move new star to location
         this.moveToRandomLoc();
         if (this.starCount === 5) {
-            player.message = "STAR POWER";
+            player.message = 'STAR POWER';
             this.starScore++;
             this.starCount--;
             this.seconds = 5;
@@ -528,7 +530,7 @@ PowerUp.prototype.collectReward = function(){
     else if (this.type === 10) {
         player.score = player.score + 10;
         player.loc = 2;
-        if(debug > 0){console.log("Teleported to next level")};
+        if(debug > 0){console.log('Teleported to next level')};
     }
 };
 
@@ -536,7 +538,7 @@ PowerUp.prototype.collectReward = function(){
 
 // Log some info to console on game load
 console.log("Welcome to Rich's game");
-console.log("Enter debug=1 or debug=2 into console to trigger debugging if desired.")
+console.log('Enter debug=1 or debug=2 into console to trigger debugging if desired.');
 
 // Configuration parameters asigned as variables
 var initialPowerUpSeconds = 20;
@@ -562,15 +564,15 @@ var powerUp = new PowerUp();
 var player = new Player(27);
 
 // Bring some enemies into the game.
-var icky = new Enemy("Icky", 7, 1, 'images/enemy-bug.png')
-var sticky = new Enemy("Sticky", 3, 2, 'images/enemy-bug.png')
-var tricky = new Enemy("Tricky", 4, 3, 'images/enemy-bug.png')
-var vicky = new Enemy("Vicky", 1, 4, 'images/enemy-bug-green.png')
-var micky = new Enemy("Micky", 6, 5, 'images/enemy-bug.png');
-var nicky = new Enemy("Nicky", 9, 6, 'images/enemy-bug.png');
-var ricky = new Enemy("Ricky", 5, 7, 'images/enemy-bug.png');
-var picky = new Enemy("Picky", 12, 8, 'images/enemy-bug.png');
-var steve = new Enemy("Steve", 30, 9, 'images/enemy-bug-red.png');
+var icky = new Enemy('Icky', 7, 1, 'images/enemy-bug.png');
+var sticky = new Enemy('Sticky', 3, 2, 'images/enemy-bug.png');
+var tricky = new Enemy('Tricky', 4, 3, 'images/enemy-bug.png');
+var vicky = new Enemy('Vicky', 1, 4, 'images/enemy-bug-green.png');
+var micky = new Enemy('Micky', 6, 5, 'images/enemy-bug.png');
+var nicky = new Enemy('Nicky', 9, 6, 'images/enemy-bug.png');
+var ricky = new Enemy('Ricky', 5, 7, 'images/enemy-bug.png');
+var picky = new Enemy('Picky', 12, 8, 'images/enemy-bug.png');
+var steve = new Enemy('Steve', 30, 9, 'images/enemy-bug-red.png');
 
 // Place all enemy objects in an array called allEnemies
 // Initial enemy set on easy mode.
@@ -582,7 +584,7 @@ var allEnemies = [icky,sticky,tricky,vicky];
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     // Modified to only print to console if debug is enabled
-    if(debug > 0){console.log("player input at "+e)};
+    if(debug > 0){console.log('player input at '+e)};
     var allowedKeys = {
         37: 'left',
         38: 'up',
